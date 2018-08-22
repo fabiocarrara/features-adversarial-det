35e0edd91fd02ff8f4abff24d3c53901fadbcfa0
Fabio Carrara <fabio.carrara@isti.cnr.it>
2018-08-22 16:02:05 +0200

    added lbfgs attack

--- attack.py
+++ attack.py
@@ -1,21 +1,17 @@
 import argparse
-import itertools
 import os
 
-import torch
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-
+import torch
 from cleverhans.attacks import FastGradientMethod, BasicIterativeMethod, MadryEtAl, MomentumIterativeMethod, DeepFool, \
-    CarliniWagnerL2, SaliencyMapMethod
+    CarliniWagnerL2, SaliencyMapMethod, LBFGS
 from cleverhans.model import CallableModelWrapper
-
 from torch.utils.data import DataLoader
 from torchvision import models
 from torchvision.transforms import ToTensor, Compose, Resize, Normalize, CenterCrop
 from tqdm import tqdm
-
-import matplotlib.pyplot as plt
 
 from utils import ImageDataset, convert_pytorch_model_to_tf
 
@@ -57,12 +53,14 @@ def main(args):
 
     dataset = ImageDataset(args.image_folder, transform=transform, return_paths=True)
     # n_images = len(dataset)
-    dataloader = DataLoader(dataset, shuffle=False, batch_size=args.batch_size, pin_memory=True, num_workers=8)
+    dataloader = DataLoader(dataset, shuffle=False, batch_size=args.batch_size, pin_memory=True, num_workers=0)
 
     model = models.resnet50(pretrained=True).to(args.device)
     model.eval()
 
-    sess = tf.Session()
+    config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1, allow_soft_placement=True,
+                            device_count={'CPU': 1})
+    sess = tf.Session(config=config)
     x_op = tf.placeholder(tf.float32, shape=(None, 3, 224, 224,))
 
     tf_model = convert_pytorch_model_to_tf(model, args.device)
@@ -99,13 +97,19 @@ def main(args):
         attack_params = {**common_params, **grad_params, **iter_params}
     elif args.attack == 'jsma':
         attack_op = SaliencyMapMethod(cleverhans_model, sess=sess)
-        attack_params = common_params
+        attack_params = {'theta': eps, 'symbolic_impl': False, **common_params}
     elif args.attack == 'deepfool':
         attack_op = DeepFool(cleverhans_model, sess=sess)
         attack_params = common_params
     elif args.attack == 'cw':
         attack_op = CarliniWagnerL2(cleverhans_model, sess=sess)
         attack_params = common_params
+    elif args.attack == 'lbfgs':
+        attack_op = LBFGS(cleverhans_model, sess=sess)
+        target = np.zeros((1, 1000))
+        target[0, np.random.randint(1000)] = 1
+        y = tf.placeholder(tf.float32, target.shape)
+        attack_params = {'y_target': y, **common_params}
 
     attack_name = args.attack + attack_name
 
@@ -122,7 +126,7 @@ def main(args):
 
         progress.set_description('ATTACK')
 
-        z, adv_x, adv_z = sess.run([preds_op, adv_x_op, adv_preds_op], feed_dict={x_op: x})
+        z, adv_x, adv_z = sess.run([preds_op, adv_x_op, adv_preds_op], feed_dict={x_op: x, y: target})
 
         src, dst = np.argmax(z, axis=1), np.argmax(adv_z, axis=1)
         success = src != dst
@@ -152,7 +156,7 @@ if __name__ == '__main__':
     parser.add_argument('image_folder', help='Directory containing images to be modified')
     parser.add_argument('out_folder', help='Prefix path for generated adversarial examples')
     parser.add_argument('-b', '--batch-size', type=int, default=1, help='Number of attacks to run simultaneously')
-    parser.add_argument('-a', '--attack', choices=('fgsm', 'iter', 'm-iter', 'pgd', 'deepfool', 'jsma', 'cw'),
+    parser.add_argument('-a', '--attack', choices=('lbfgs', 'fgsm', 'iter', 'm-iter', 'pgd', 'deepfool', 'jsma', 'cw'),
                         default=20,
                         help='Number of attacks to run simultaneously')
     parser.add_argument('-e', '--eps', type=int, default=20, help='Maximum perturbation (in range [0, 255])')
